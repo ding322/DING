ac70fda54883aeaacf8fb78345af9323ffcd26df
ding322 <32032424+ding322@users.noreply.github.com>
2017-10-15 22:38:11 +0800

    Add files via upload

---
+++ exer3-3.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+void main()
+{
+	int x,leap;
+	cout<<"please input x:"<<endl;
+	cin>>x;
+	if(x%3==0||x%7==0)
+		leap=1;
+	else 
+		leap=0;
+	if(leap==1)
+		cout<<"YES"<<endl;
+	else
+		cout<<"NO"<<endl;
+}
